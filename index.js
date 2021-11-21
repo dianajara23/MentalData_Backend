785fbc4b7699bec863c1189ca8e6ebf00f9af477
dianajara23 <dpjara23@ucatolica.edu.co>
2021-11-21 16:18:13 -0500

    Creating Node.js project with Express and PostgreSQL connection

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+const port = 3000
+
+// Crear el servidor
+const app = express()
+
+
+// body-parser extrae toda la parte del cuerpo de una secuencia de solicitud entrante y la expone en req.body. 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// habilitar cors
+// app.use(cors());
+
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres MentalData' })
+})
+
+app.get('/users', db.getUsers )
+app.get('/users/:id', db.getUserById)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
